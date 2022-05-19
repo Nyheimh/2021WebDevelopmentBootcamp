59c303544375c594d92a72bb4ab9bdd202f0a2a7
Nyheim Hunter <nyheimhunter@Nyheims-MacBook-Air.local>
2022-05-19 15:21:28 -0700

    comparatives

---
+++ Javascript/intermediateJavascript/randomNumberGenerator.js
@@ -0,0 +1,19 @@
+// Random number generator
+
+var n = Math.random()
+n = Math.floor(n * 6) + 1
+// console.log(n)
+
+alert("Hello, I will be calculating your love today. I will need a few things before we start")
+var personOne = prompt("What is your name?")
+var personTwo = prompt("What is their name?")
+
+var num = Math.random
+num = Math.floor(n * 100) + 1
+console.log(num)
+
+if ( num > 75){
+  alert("Your love score is " + num + " what are the chances")
+} else {
+  alert(personOne + " + " + personTwo + "  has a " + num + "%, chance to fall in love.")
+}
