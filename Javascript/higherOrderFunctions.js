cfcc59a71fd501c95ab0ffbb52160e2fa5e208ea
Nyheim Hunter <nyheimhunter@Nyheims-MacBook-Air.local>
2022-06-24 15:55:56 -0700

    Formatting

--- Javascript/higherOrderFunctions.js
+++ Javascript/higherOrderFunctions.js
@@ -17,3 +17,31 @@ function divide(num1, num2) {
 function calculator(num1, num2, operator) {
   return operator(num1, num2);
 }
+
+// Constructor Function
+function BellBoy(name, age, hasWorkPermit, languages) {
+  this.name = name;
+  this.age = age;
+  this.hasWorkPermit = hasWorkPermit;
+  this.languages = languages;
+  this.moveSuitcase = function () {
+    alert("May I take your suitcase?");
+    pickUpSuitcase();
+    move();
+  };
+}
+
+// Initialise Object
+var bellBoy1 = new BellBoy("Timmy", 19, true, ["French", "English"]);
+
+function HouseKeeper(name, yoe, cleaningRepertoire) {
+  this.name = name;
+  this.yoe = yoe;
+  this.cleaningRepertoire = cleaningRepertoire;
+}
+
+var houseKeeper1 = new HouseKeeper("Wilma", 25, [
+  "kitchen",
+  "livingroom",
+  "dining room",
+]);
